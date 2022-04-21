b3190fb2bfea03fe950e11a660fcc9fc62168979
goni1621 <lpark3955@gmail.com>
2022-04-21 14:48:41 +0900

    Create client

---
+++ info-market/client/src/App copy.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        client 최초 레파지토리
+      </header>
+    </div>
+  );
+}
+
+export default App;
