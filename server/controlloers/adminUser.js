7acaeda8a3303f04a24972119549c3119182ac31
wlgns1501 <wlgns1501@gmail.com>
2022-04-22 15:36:29 +0900

    server admin

---
+++ server/controlloers/adminUser.js
@@ -0,0 +1,57 @@
+const userDb = require('../db/user.js');
+
+module.exports = {
+  getUsers: async (req, res) => {
+    const { pages, limit } = req.params;
+
+    const users = await userDb.findUsers(pages, limit).catch(() => {
+      return res
+        .status(400)
+        .json({ message: '회원 정보를 불러오는데 실패했습니다.' });
+    });
+
+    if (users.count === 0) {
+      return res.status(406).json({ message: '유저가 존재하지 않습니다.' });
+    }
+
+    res.status(200).json({ users, message: '가입한 유저들을 불러왔습니다.' });
+  },
+  editUserInfo: async (req, res) => {
+    const { userId } = req.params;
+    const { email, nickname, point, grade } = req.body;
+    const user = await userDb.findUser(userId);
+
+    if (!user) {
+      return res.status(406).message({ message: '유저가 존재하지 않습니다.' });
+    }
+
+    const editUser = await userDb
+      .editUserInfo(userId, email, nickname, point, grade)
+      .catch(() => {
+        return res
+          .status(400)
+          .json({ message: '회원 정보를 수정하는데 실패했습니다.' });
+      });
+
+    res
+      .status(200)
+      .json({ editUser, message: '해당 유저의 정보를 수정 했습니다.' });
+  },
+  removeUser: async (req, res) => {
+    const { userId } = req.params;
+
+    const user = await userDb.findUser(userId);
+
+    if (!user) {
+      return res.status(406).message({ message: '유저가 존재하지 않습니다.' });
+    }
+
+    await userDb.removeUser(userId).catch(() => {
+      return res
+        .status(400)
+        .json({ message: '회원을 삭제 하는데 실패했습니다.' });
+    });
+
+    res.status(200).json({ message: '해당 유저를 삭제했습니다.' });
+  },
+};
--- server/routes/admin.js
+++ server/routes/admin.js
@@ -1,10 +1,16 @@
 const express = require('express');
 const router = express.Router();
-const adminControllers = require('../controlloers/admin');
+const adminUserControllers = require('../controlloers/adminUser');
+const adminInfoControllers = require('../controlloers/adminInfo');
 
-// router.post('/login', adminControllers.login);
-// router.post('/logout', adminControllers.logout);
-// router.post('/signup', adminControllers.signup);
-// router.delete('/:userId', adminControllers.remove);
+// 유저 관리
+router.get('/users', adminUserControllers.getUsers);
+router.put('/users/:userId', adminUserControllers.editUserInfo);
+router.delete('/users/:userId', adminUserControllers.removeUser);
+
+// 게시물 관리
+router.get('/info', adminInfoControllers.getInfo);
+router.put('/info/:infoId', adminInfoControllers.editInfo);
+router.delete('/info', adminInfoControllers.removeInfo);
 
 module.exports = router;
