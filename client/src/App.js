bc2bae1c9beaff8cead7cf774bcabb5c36cbad34
goni1621 <lpark3955@gmail.com>
2022-04-26 03:00:55 +0900

    Fix [login, signup, oauth]

--- client/src/App.js
+++ client/src/App.js
@@ -6,8 +6,39 @@ import { createStore } from 'redux';
 import Login from './pages/Login';
 import Tos from './pages/Tos'
 import Signup from './pages/Signup'
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+
 
 function App() {
+
+  const [isLogin, setIsLogin] = useState(false);
+  const [userInfo, setUserInfo] = useState({});
+
+  // const T = [];
+
+  /* 로그인상태 변경, 메인페이지 불러옴 */
+
+  // /* 로그인 요청 성공 */
+  // const handleResponseSuccess = () => {
+  //   setIsLogin(true);
+  // };
+
+  // /* 로그아웃 */
+  // const handleLogout = () => {
+  //   axios.post(`https://localhost:3000/auth/logout`).then((res) => {
+  //     setUserInfo(null);
+  //     console.log(userInfo);
+  //     setIsLogin(false);
+  //   });
+  // };
+
+  useEffect(() => {
+    console.log("before", userInfo);
+    // isAuthenticated();
+    console.log("after", userInfo);
+  }, []);
+
 
   return (
     <BrowserRouter>
