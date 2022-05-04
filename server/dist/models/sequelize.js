05b0e8ce7a9e3387f32d0c9f1a1efbc6630fde68
dbstjs95 <choji95@naver.com>
2022-05-04 11:48:38 +0900

    커밋

--- server/dist/models/sequelize.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.sequelize = void 0;
-const sequelize_1 = require("sequelize");
-const config = require('../config/config');
-const env = process.env.NODE_ENV ||
-    'development';
-const { database, username, password } = config[env];
-exports.sequelize = new sequelize_1.Sequelize(database, username, password, config[env]);
