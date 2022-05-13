5a57158b909791fb365c5fe570dd8167afe2e24f
wlgns1501 <wlgns1501@gmail.com>
2022-05-13 17:17:40 +0900

    server [feature] info edit

--- server/src/models/sequelize.ts
+++ server/src/models/sequelize.ts
@@ -1,10 +1,15 @@
 import { Sequelize } from 'sequelize';
 const config = require('../config/config');
+// import '../config/config';
+
+// console.log(config);
 
 const env =
   (process.env.NODE_ENV as 'production' | 'test' | 'development') ||
   'development';
 const { database, username, password } = config[env];
+
+console.log(config[env]);
 
 export const sequelize = new Sequelize(
   database,
